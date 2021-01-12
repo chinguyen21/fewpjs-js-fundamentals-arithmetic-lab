08498a1931e21512bf642041c5cf3dbb65562069
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-01-11 23:19:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+  let num1 = 1;
+  let num2 = 62;
+
+const multiply = num1 * num2
+
+const random = Math.ceil(Math.random() * 10)
+
+const mod = 9 % 5
+
+const max = Math.max(2,3,20,4)
